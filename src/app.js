b717e18657c193a77dbde8dd248732d60d86d6a6
Richard <fahitch+github@gmail.com>
2019-11-26 18:18:39 +0000

    finally gets the ruddy fetch working

---
+++ src/app.js
@@ -0,0 +1,26 @@
+import Vue from 'vue';
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  new Vue ({
+    el: "#app",
+    data: {
+      currencies:[],
+      selectedCurrency: null,
+      targetCurrency: null
+    },
+    // computed: {},
+    mounted(){
+      this.getCurrencies()
+    },
+    methods: {
+      getCurrencies: function(){
+        fetch("https://api.exchangeratesapi.io/latest")
+        .then(response => response.json)
+        .then(data => this.currencies = data)
+      }
+    },
+
+  })
+
+})
